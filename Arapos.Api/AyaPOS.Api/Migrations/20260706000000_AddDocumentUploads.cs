893630291fe8748206cdb4d7c84264d68b7d07e9
ayarahall <51592371+ayarahall@users.noreply.github.com>
2026-07-06 22:33:05 +0400

    s

--- Arapos.Api/AyaPOS.Api/Migrations/20260706000000_AddDocumentUploads.cs
+++ Arapos.Api/AyaPOS.Api/Migrations/20260706000000_AddDocumentUploads.cs
@@ -1,6 +1,7 @@
 using System;
 using Ayapos.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
